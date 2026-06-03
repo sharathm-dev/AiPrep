92e32013355b2150c2b0729c78408e327afaedf8
sharathm-dev <sharathstudiespg@gmail.com>
2026-06-03 11:39:51 +0530

    Update CORS to support FRONTEND_URL environment variable

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["content-type", "Authorization"],
   })
